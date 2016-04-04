e10f488f005a4d7f63be5d2e338050cd9cbc5083
ErikNovak <novak.erik@ijs.si>
2016-04-04 13:24:02 +0200

    Made some modifications to the autocomplete. Added a footer with the eu-commission picture and description.

--- dashboard/live/js/autocomplete.js
+++ dashboard/live/js/autocomplete.js
@@ -1,9 +1,13 @@
 ﻿
 /**
  * Functionality of the autocomplete skill search.
+ * 
+ * Uses typeahead.js: https://twitter.github.io/typeahead.js/
+ * and Bootstrap Tags Input: http://bootstrap-tagsinput.github.io/bootstrap-tagsinput/examples/
  */ 
 
 $(function () {
+    // skillset gathered from the provided data map_dump.csv
     var skillset = ['Python', 'Advanced computing', 'Programming', 'Computational systems', 'Coding', 'Cloud computing', 'Databases', 
         'Data management', 'Data engineering', 'Data mining', 'Data formats', 'Linked data', 'Information extraction', 'Stream processing', 
         'Enterprise process', 'Business intelligence', 'Data anonymisation', 'Semantics', 'Schema', 'Data licensing', 'Data quality', 
@@ -12,7 +16,7 @@ $(function () {
         'Data analysis', 'Data visualisation', 'Infographics', 'Interaction', 'Data mapping', 'Data stories', 'Data journalism', 'D3js', 
         'Tableau'];
     
-    // author typeahead and tags 
+    // skills typeahead and tags 
     var skills = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
